d61ea58a6e5c0c6cb1747bee0e557ce337f62c10
evylagi <dmikeaecyl@gmail.com>
2026-08-11 13:34:50 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "8107617495:AAEjCpxJ0qVmG1m7C5rzAU_maM2t9IlnUJs")
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "8586105261:AAGmyTRAZzQACC_OIee-nGDTQTRBowfz78Y")
 FISH_API_KEY = os.environ.get("FISH_API_KEY", "sk-fish-2IfHrnq1IG3lhnGoCFVbiNwRrdoR_yM4OXZEb7KfO_g")
 
 BOT_NAME = "VoiceStudio Pro"
